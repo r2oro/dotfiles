e7f9f1486c40ef57657431554552f2c38b99082c
212050466 <josh.schneider@ge.com>
2016-01-20 12:42:27 -0600

    added sudo for gem

--- lib.sh
+++ lib.sh
@@ -71,7 +71,7 @@ function require_gem() {
     running "gem $1"
     if [[ $(gem list --local | grep $1 | head -1 | cut -d' ' -f1) != $1 ]];
         then
-            action "gem install $1"
+            action "sudo gem install $1"
             gem install $1
     fi
     ok
